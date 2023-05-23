39b7fa05b795d25b30edf0ff3b69af930daf5fb6
Shubham <shubham11729@gmail.com>
2023-05-23 14:15:17 -0500

    Readme fixed

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -4,18 +4,35 @@ import { useNavigate } from 'react-router-dom';
 
 const Search = () => {
   const [username, setUsername] = useState('');
+  const [error, setError] = useState('');
   const navigate = useNavigate();
 
   const handleInputChange = (e) => {
     setUsername(e.target.value);
+    setError('');
   };
 
   const handleSubmit = (e) => {
     
     e.preventDefault();
     if (username.trim() !== '') {
-      navigate(`/gh_finder/user/${username}`);
+     
+      if (isValidUsername(username)) {
+        navigate(`/gh_finder/user/${username}`);
+      } else {
+        setError('Invalid username. Please enter a valid username.');
+      }
+    } else {
+      setError('Please enter a username.');
     }
+  };
+
+  const isValidUsername = (username) => {
+    const usernameRegex = /^[a-zA-Z0-9][a-zA-Z0-9-]{1,}[a-zA-Z0-9]$/;
+    return (
+      username.length >= 3 &&
+      username.match(usernameRegex) !== null
+    );
   };
 
   return (
@@ -31,6 +48,7 @@ const Search = () => {
         />
         <input type="submit" value="Search"/>
       </form>
+      {error && <p className="error">{error}</p>}
     </div>
   );
 };
